da95e81f77ab243aa8447c66a80b32eaf914a00f
Maharshi-Mistry <mistryguru@gmail.com>
2024-10-22 23:54:14 +0530

    Support files
    command and support files

---
+++ commands.js
@@ -0,0 +1,38 @@
+// ***********************************************
+// This example commands.js shows you how to
+// create various custom commands and overwrite
+// existing commands.
+//
+// For more comprehensive examples of custom
+// commands please read more here:
+// https://on.cypress.io/custom-commands
+// ***********************************************
+//
+//
+// -- This is a parent command --
+// Cypress.Commands.add('login', (email, password) => { ... })
+//
+//
+// -- This is a child command --
+// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
+//
+//
+// -- This is a dual command --
+// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
+//
+//
+// -- This will overwrite an existing command --
+// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+/// <reference types="cypress" />
+/// <reference types="cypress-xpath" /> 
+// Created my own command
+
+Cypress.Commands.add('GetIframe', (Iframe)=>{
+    return cy.get('Iframe')
+    .its('0.contentDocument.body')
+    .should('be.visible')
+    .then(cy.wrap)
+})
+
+//imported iframe commands by installing plugin and describe in the command file
+require('cypress-iframe')
