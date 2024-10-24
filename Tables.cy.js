78f1f31a340f0063ff00088c1f0bd9634bf87d38
Maharshi-Mistry <mistryguru@gmail.com>
2024-10-25 00:30:36 +0530

    Update Tables.cy.js

--- Tables.cy.js
+++ Tables.cy.js
@@ -22,6 +22,17 @@ describe('Tables check', function(){
         cy.get("table[class='table table-bordered table-hover']>tbody>tr:nth-child(1) td:nth-child(1)").click() //to check the check box from the table
         cy.get("table[class='table table-bordered table-hover']>tbody>tr:nth-child(1) td:nth-child(2)").next().should('have.contain','min1i@gmail.com')
         cy.get("table[class='table table-bordered table-hover']>thead>tr:nth-child(1) td:nth-child(2)").should('have.text','Customer Name')//count each column
-
+    })
+    it('Table record from the rows and columns', function(){
+        //how to read the all data from the table from first page
+        cy.get("table[class='table table-bordered table-hover']>tbody>tr").should('have.contain','10')//Count each row
+         .each( ($row, index, $rows)=>{   //this each is for to get row store in $rows then count should be store in index and one by one it will store in $row
+            cy.wrap($row).within( ()=>
+            {
+                cy.get("td").each( ($column, index, $columns) =>{  //this to get each td and the each is for to get columns store in $columns then count should be store in index and one by one it will store in $columns
+                    cy.log($column.text()) //log is use to log the data and .text() is use to get print the text
+                })
+            })
+         })
     })
 })
