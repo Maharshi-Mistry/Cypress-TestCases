1da6f96a74ab73aa373a41dd7b5983f74adf206e
Maharshi-Mistry <mistryguru@gmail.com>
2024-10-25 01:17:59 +0530

    Update Tables.cy.js

--- Tables.cy.js
+++ Tables.cy.js
@@ -35,4 +35,40 @@ describe('Tables check', function(){
             })
          })
     })
+    it.only('Table record from the rows and columns', function(){
+        //how to read the all data from the table from mulitple pages
+        //find total number of pages
+        /*let totalpages;
+        cy.get(".col-sm-6.text-end").then( (e)=>{
+            let text = e.text() //text variable now contains the total number of pages
+            totalpages = text.substring( text.indexOf('(')+1, text.indexOf('Pages')-1);
+            cy.log("The total number of pages in the table=> "+totalpages);
+        })
+    */
+    let totalpage = 10;
+    for(let p=1;p<=totalpage;p++)
+    {
+        if(totalpage>1)
+        {
+            cy.log('Active Page = ' +p);
+            cy.get("ul[class='pagination']>li:nth-child("+p+")").click()
+            cy.wait(1000)
+            cy.get("table[class='table table-bordered table-hover']>tbody>tr").should('have.contain','10')//Count each row
+
+            //Loop to get all the data and print
+             .each( ($row, index, $rows)=>{   //this each is for to get row store in $rows then count should be store in index and one by one it will store in $row
+                cy.wrap($row).within( ()=>
+                {
+                    cy.get("td:nth-child(3)").then((e) =>{  //this to get each 3 td(which is email in this case) and the each is for to get columns store in $columns then count should be store in index and one by one it will store in $columns
+                    cy.log(e.text()) //log is use to log the data and .text() is use to get print the text
+                    })
+                })
+            
+            }) 
+
+
+        }
+    }
+    })
+
 })
